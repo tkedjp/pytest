8ed1ac0e801c4f5931c406d8e442b182324df7f5
tkedjp <120151701+tkedjp@users.noreply.github.com>
2023-02-23 21:23:18 +0900

    3rd

--- tests/test_code3.py
+++ tests/test_code3.py
@@ -10,7 +10,7 @@ def get_json_data_mock(id):
 def test_get_user_names(monkeypatch):
     monkeypatch.setattr(
         code3, 'get_json_data', get_json_data_mock)
-    result = code3.get_user_names(['001','009'])
+    result = code3.get_user_names(['001'])
 
-    assert list(result.keys()) == ['001','009']
-    assert list(result.values()) == ['太郎','太郎']
+    assert list(result.keys()) == ['001']
+    assert list(result.values()) == ['太郎']
