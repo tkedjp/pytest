509ee319d6cb2215fc7cfee8803ee7d50f17a33e
tkedjp <120151701+tkedjp@users.noreply.github.com>
2023-02-23 21:16:07 +0900

    2nd

---
+++ tests/test_code3.py
@@ -0,0 +1,16 @@
+from src import code3
+
+def get_json_data_mock(id):
+    return{'name':'太郎'}
+
+# monkeypathcの3つの引数
+# 1のcode3はmockに差し替えたい関数のモジュール
+# 2のget_json_dataはmockに差し替えたい関数名の文字列
+# 3のget_json_data_mockは実際に置き換えるmockの関数
+def test_get_user_names(monkeypatch):
+    monkeypatch.setattr(
+        code3, 'get_json_data', get_json_data_mock)
+    result = code3.get_user_names(['001','009'])
+
+    assert list(result.keys()) == ['001','009']
+    assert list(result.values()) == ['太郎','太郎']
--- tests/test_func.py
+++
@@ -1,6 +0,0 @@
-# test_func.py
-def add_one(n):
-    return n + 1
- 
-def add_one_test():
-    assert add_one(3) == 4
