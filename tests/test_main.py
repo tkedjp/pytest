b0a9dc33c5988302cb0cf85b45fb16e01f10efbc
tkedjp <120151701+tkedjp@users.noreply.github.com>
2023-02-28 08:26:59 +0900

    2nd

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 from src import main
 
 def test_func_main(mocker):
-    mocker.patch('main_func_hoge', return_value=[1, 2])
+    mocker.patch('src.main.func_hoge', return_value=[1, 2])
     print(main.func_main()) # [1, 2]
