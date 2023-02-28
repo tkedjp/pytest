3e1cb4ecf19a826c57851c2bfa99abc77d4e5299
tkedjp <120151701+tkedjp@users.noreply.github.com>
2023-02-28 10:39:01 +0900

    4th

--- tests/test_code3.py
+++ tests/test_code3.py
@@ -3,7 +3,7 @@ from src import code3
 def get_json_data_mock(id):
     return{'name':'太郎'}
 
-# monkeypathcの3つの引数
+# monkeypathの3つの引数
 # 1のcode3はmockに差し替えたい関数のモジュール
 # 2のget_json_dataはmockに差し替えたい関数名の文字列
 # 3のget_json_data_mockは実際に置き換えるmockの関数
