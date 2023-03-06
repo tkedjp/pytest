7e4648d5ba24629c48a3f72d86065e86dd705eb0
tkedjp <120151701+tkedjp@users.noreply.github.com>
2023-03-06 17:32:36 +0900

    2nd

--- tests/test_salary.py
+++ tests/test_salary.py
@@ -1,10 +1,11 @@
 import unittest
 from unittest.mock import MagicMock
 
-import src.salary
+import src.salary as salary
 
 class TestSalary(unittest.TestCase):
     def test_calculation_salary(self):
         s = salary.Salary(year=2017)
         s.bonus_api.bonus_price = MagicMock(return_value=1)
         self.assertEqual(s.calculation_salary(), 101)
+        s.bonus_api.bonus_price.assert_called()
