45d5e894814c398ab3f30b39026ff4beb4430448
tkedjp <120151701+tkedjp@users.noreply.github.com>
2023-03-07 10:01:43 +0900

    1st

---
+++ tests/test_calc.py
@@ -0,0 +1,15 @@
+class TestCalculation(object):
+
+    # 各テストの開始前に実行
+    def setup_method(self, method):
+        print('テスト開始前')
+
+    # 各テストの開始後に実行
+    def teardown_method(self, method):
+        print('テスト開始後')
+
+    def test_1(self):
+        print('test1')
+
+    def test_2(self):
+        print('test2')
