60f422266d111584f0a1a222951b23bcc69e0072
tkedjp <120151701+tkedjp@users.noreply.github.com>
2023-03-13 15:24:59 +0900

    4th

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,7 @@ from src import main
 
 #pytest-mockをインストールすれば、fixtureでmockerが使えるようになる
 #差し替えたい処理をpatchで指定してモックにする
+#以下は戻り値としてreturn_valueを指定している。戻り値がない場合は、指定しなくてもよい
 
 def test_func_main(mocker):
     mocker.patch("src.main.func_hoge", return_value=[1, 2])
